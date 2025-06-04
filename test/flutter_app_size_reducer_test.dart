c8d352483bdaf755e517fd13da1dde2f185c57cf
Satya Prakash <satyaprakash6945@gmail.com>
2025-06-04 16:59:35 +0530

    Add: test case

--- test/flutter_app_size_reducer_test.dart
+++ test/flutter_app_size_reducer_test.dart
@@ -1,1 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_app_size_reducer/flutter_app_size_reducer.dart';
 
+void main() {
+  group('FlutterAppSizeReducer Tests', () {
+    test('package initialization', () {
+      expect(FlutterAppSizeReducer, isNotNull);
+    });
+  });
+}
